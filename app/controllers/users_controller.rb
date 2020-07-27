108361ad0c0601ea167bde9e2a025ae9f6e0429c
grace-mugoiri <gracemugoiri@gmail.com>
2020-07-27 11:42:19 +0300

    add users controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = user.new(user_params)
+		if @user.save
+			flash[:success] = 'Welcome to the app!'
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :password_conirmation)
+	end
 end
