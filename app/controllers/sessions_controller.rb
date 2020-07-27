9ad696db3dd50b6e903bbd89e1f869d656abd673
grace-mugoiri <gracemugoiri@gmail.com>
2020-07-27 13:32:24 +0300

    add session controller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		user = user.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			log_in user
+			redirect_back_or user
+		else
+			flash.now[:danger] = 'Invalid emial/password combination'
+			render 'new'
+		end
+	end
+
+	def destroy
+		log_out
+		redirect_to root_url
+	end
 end
